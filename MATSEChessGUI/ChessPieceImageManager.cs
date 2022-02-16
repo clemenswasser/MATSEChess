b20b15d1ccca4f2cc2f378a98f7a3af114873d2f
Clemens Wasser <clemens.wasser@gmail.com>
2022-02-16 16:24:20 +0100

    Extract ResourcePath

--- MATSEChessGUI/ChessPieceImageManager.cs
+++ MATSEChessGUI/ChessPieceImageManager.cs
@@ -8,20 +8,22 @@ namespace MATSEChessGUI
 {
     public class ChessPieceImageManager
     {
+        private static string ResourcePath(string name) => $"/resources/{name}";
+
         private static Dictionary<ChessPieceInstance, string> imageResourcePaths = new Dictionary<ChessPieceInstance, string>()
         {
-            { new ChessPieceInstance(ChessPieceType.BISHOP, ChessColor.BLACK), "/resources/Chess_bdt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.BISHOP, ChessColor.WHITE), "/resources/Chess_blt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.KING, ChessColor.BLACK), "/resources/Chess_kdt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.KING, ChessColor.WHITE), "/resources/Chess_klt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.KNIGHT, ChessColor.BLACK), "/resources/Chess_ndt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.KNIGHT, ChessColor.WHITE), "/resources/Chess_nlt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.PAWN, ChessColor.BLACK), "/resources/Chess_pdt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.PAWN, ChessColor.WHITE), "/resources/Chess_plt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.QUEEN, ChessColor.BLACK), "/resources/Chess_qdt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.QUEEN, ChessColor.WHITE), "/resources/Chess_qlt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.ROOK, ChessColor.BLACK), "/resources/Chess_rdt45.svg" },
-            { new ChessPieceInstance(ChessPieceType.ROOK, ChessColor.WHITE), "/resources/Chess_rlt45.svg" },
+            { new ChessPieceInstance(ChessPieceType.BISHOP, ChessColor.BLACK), ResourcePath("Chess_bdt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.BISHOP, ChessColor.WHITE), ResourcePath("Chess_blt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.KING, ChessColor.BLACK), ResourcePath("Chess_kdt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.KING, ChessColor.WHITE), ResourcePath("Chess_klt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.KNIGHT, ChessColor.BLACK), ResourcePath("Chess_ndt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.KNIGHT, ChessColor.WHITE), ResourcePath("Chess_nlt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.PAWN, ChessColor.BLACK), ResourcePath("Chess_pdt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.PAWN, ChessColor.WHITE), ResourcePath("Chess_plt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.QUEEN, ChessColor.BLACK), ResourcePath("Chess_qdt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.QUEEN, ChessColor.WHITE), ResourcePath("Chess_qlt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.ROOK, ChessColor.BLACK), ResourcePath("Chess_rdt45.svg") },
+            { new ChessPieceInstance(ChessPieceType.ROOK, ChessColor.WHITE), ResourcePath("Chess_rlt45.svg") },
         };
 
         private static Dictionary<(ChessPieceInstance, int), Bitmap> imageCache = new Dictionary<(ChessPieceInstance, int), Bitmap>();
